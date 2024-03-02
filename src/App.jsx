55d7cc566c19735b0276f0dd2303ab8f14802005
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-02 15:58:07 +0330

    In the name of god, starting point.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3 className='py-4 w-100 bg-red-500 text-center text-white text-2xl'>
+      In the name of god...
+      </h3>
+    </div>
+  )
+}
+
+export default App
