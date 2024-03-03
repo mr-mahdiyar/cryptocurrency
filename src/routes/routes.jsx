669dbe5a7fd1437fe9997fb6483d3f5974f784d7
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-03 16:58:38 +0330

    All routes were added.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
-import { App } from "../components/";
-
+import { CryptocurrenciesPage, CryptoDetailsPage, HomePage, ExchangesPage, App, News } from "..";
 const routes = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/cryptocurrenciesPage",
+                element: <CryptocurrenciesPage />
+            },
+            {
+                path: "/cryptoDetailsPage",
+                element: <CryptoDetailsPage />
+            },
+            {
+                path: "/exchangesPage",
+                element: <ExchangesPage />
+            },
+            {
+                path: "/news",
+                element: <News />
+            },
+        ]
     }
 ])
 export default routes;
