e7fbe72e68b0794cb51a222a4c225fcde35d7c73
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-03 16:53:27 +0330

    All Pages were added to index.

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./components/Navbar";
+export { default as App } from "./App";
+export { default as CryptocurrenciesPage } from "./Pages/CryptoDetailsPage";
+export { default as CryptoDetailsPage } from "./Pages/CryptoDetailsPage";
+export { default as ExchangesPage } from "./Pages/ExchangesPage";
+export { default as HomePage } from "./Pages/HomePage";
+export { default as News } from "./Pages/News";
