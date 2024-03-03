bb1ac5c6b3e2e83379685e32a61e5bfad967eabd
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-03 16:45:22 +0330

    routes were created and imported to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+import routes from "./routes/routes.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter routes={routes}>
     <App />
   </BrowserRouter>
 );
