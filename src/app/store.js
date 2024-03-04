9b600370504d6a56cf476d003a4e8a27ce5fdd8d
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-04 16:53:47 +0330

    middleware was added to store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { cryptoApi } from "../services/cryptoApi";
+
 const store = configureStore({
-  reducer: { 
-    [cryptoApi.reducerPath]: cryptoApi.reducer;
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export default store;
