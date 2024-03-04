12fe502322b7e75af258e0a42e5e75475e836b11
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-04 16:59:06 +0330

    store was added to main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import routes from "./routes/routes.jsx";
-import 'antd/dist/antd.css'
+import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "./app/store.js"; 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes} />
+  <Provider store={store}>
+    <RouterProvider router={routes} />
+  </Provider>
 );
