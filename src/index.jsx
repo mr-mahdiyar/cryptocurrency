1197c138a4bc2d0c7f2767d784b7dbe64246c49b
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-05 10:45:10 +0330

    cryptocurrenciesPage addres was editted.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 export { default as Navbar } from "./components/Navbar";
 export { default as App } from "./App";
-export { default as CryptocurrenciesPage } from "./Pages/CryptoDetailsPage";
+export { default as CryptocurrenciesPage } from "./Pages/CryptocurrenciesPage";
 export { default as CryptoDetailsPage } from "./Pages/CryptoDetailsPage";
 export { default as ExchangesPage } from "./Pages/ExchangesPage";
 export { default as HomePage } from "./Pages/HomePage";
