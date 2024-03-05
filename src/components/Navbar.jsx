58c1df5bf5fd398023b92d295fc4090e3cf9e23e
Mahdiyar <m.poursalari3200@gmail.com>
2024-03-05 10:36:52 +0330

    Key value was added to each menu items.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,16 +22,16 @@ const Navbar = () => {
         </Button> */}
       </div>
       <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
+        <Menu.Item icon={<HomeOutlined />} key={1}>
             <Link to="/">Home</Link>
         </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
+        <Menu.Item icon={<FundOutlined />} key={2}>
             <Link to="/cryptocurrencies">cryptocurrencies</Link>
         </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
+        <Menu.Item icon={<MoneyCollectOutlined />} key={3}>
             <Link to="/exchanges">exchanges</Link>
         </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
+        <Menu.Item icon={<BulbOutlined />} key={4}>
             <Link to="/news">news</Link>
         </Menu.Item>
       </Menu>
